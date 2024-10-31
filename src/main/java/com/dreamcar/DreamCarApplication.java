24f57a7eeb27967ca129904354e4afcbb6757e68
Robert <robertkania@spoko.pl>
2024-10-31 18:18:26 +0100

    Initial commit

---
+++ src/main/java/com/dreamcar/DreamCarApplication.java
@@ -0,0 +1,13 @@
+package com.dreamcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamCarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dreamcar/DreamCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamCarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
