2e4043ab422915e13b393e3ffdad970e2baaecfa
Robert <robertkania@spoko.pl>
2024-11-05 20:48:28 +0100

    Implement offers listing on the main page

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,3 +1,4 @@
+navigateTo('index');
 
 async function navigateTo(page) {
     const content_div = document.getElementById('content');
@@ -5,21 +6,60 @@ async function navigateTo(page) {
 
     try {
         const response = await fetch(`../${page}.html`);
-    
-        if(!response.ok) {
-            throw new error("Page not found!");
-        }
+
+        if(!response.ok) throw new error("Page not found!");
 
         const data = await response.text();
 
         if(page === "index") {
-            index_div[0].innerHTML = data;            
+            index_div[0].innerHTML = data;
+            loadAllOffers();
         } else {
             content_div.innerHTML = data;
         }
     } catch (error) {
-        content_div.innerHTML = `<h2>${error.message}!!!!</h2>`;
+        content_div.innerHTML = `<h2>${error.message}!</h2>`;
     }
 }
 
-navigateTo('index');
+function loadAllOffers() {
+    const content_div = document.getElementById('content');
+    content_div.innerHTML = '';
+
+    fetch("/allOffers").then(response => {
+            if(response.ok) return response.json();
+            throw new Error("blad przy ladowaniu ofert");
+        }).then(offers => {
+            displayOffers(offers);
+    }).catch(error => {
+        content_div.innerHTML = error.message;
+    });
+}
+
+function displayOffers(offers) {
+    const content_div = document.getElementById('content');
+
+    offers.forEach(offer => {
+        const offerArticle = document.createElement('article');
+        offerArticle.classList.add('offer');
+        offerArticle.innerHTML = `
+         <a href="#" onclick="navigateTo('offer-details')">
+             <div class="img">
+                 <img src="/img/car.jpg" alt="">
+             </div>
+             <div class="details">
+                 <h2>${offer.title}</h2>
+                 <ul>
+                    <li>${offer.gearbox.name}</li>
+                    <li>${offer.fuel.name}</li>
+                    <li>${offer.mileage}</li>
+                    <li>${offer.year}</li>
+                 </ul>
+                 <p class="price"><span>${offer.price}</span>pln</p>
+             </div>
+         </a>`;
+         content_div.appendChild(offerArticle);
+     });
+}
+
+
